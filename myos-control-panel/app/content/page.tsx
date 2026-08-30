2a6d3b38f6a063203e7349daf46868919a797cde
mo-yase99 <122826741+mo-yase99@users.noreply.github.com>
2026-08-30 11:34:21 -0700

    feat: add unified Content view

---
+++ myos-control-panel/app/content/page.tsx
@@ -0,0 +1,2 @@
+import GeneratedItems from '@/components/myos/generated-items'
+export default function Content(){return <main className="content"><GeneratedItems type="content" title="Content" icon="📣"/></main>}
