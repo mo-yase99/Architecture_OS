fa995a90643662be8fc882303162dc4578831949
mo-yase99 <122826741+mo-yase99@users.noreply.github.com>
2026-08-30 11:34:10 -0700

    feat: add unified Learning view

---
+++ myos-control-panel/app/learning/page.tsx
@@ -0,0 +1,2 @@
+import GeneratedItems from '@/components/myos/generated-items'
+export default function Learning(){return <main className="content"><GeneratedItems type="learning" title="Learning" icon="🎓"/></main>}
